69045b791da98b7d1403cf1a6678a35d74a42541
GrendGreen <65673679+GrendGreen@users.noreply.github.com>
2021-08-07 18:45:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FAST-STEAM</title>
+	<meta charset="utf-8">
+	<link href="img/favicon.png" rel="shorcut icon" type="image/png">
+    <link href="main.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Vast+Shadow&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+					<body>   
+
+                              <div class="topnav">
+
+
+                               <a href="garant.html">О нас</a>
+                               <a href="buy.html">Как купить?</a>
+                               <a href="help.html">Поддержка</a>   
+
+                          	        </div>
+
+
+						     <div class="logo">
+
+                            <h1> FAST-<mark>STEAM<mark> </h1>
+
+                                  </div>
+
+                                 <div id="block1">
+                                   <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">PLAYERUNKNOWN'S BATTLEGROUNDS</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/pubg.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код покупке:</span>  <span class="colortext">5glS1kSmPLs</span></p>
+                                            <p>Как купить смотрите <a href="#">тут</a>.
+                                             </td>
+									  </tr>
+									 </table>
+									     	 </div>
+
+									   <div id="block2">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Rust</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/rust.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">pg80smpS29</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+                                            
+
+									   <div id="block3">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">DayZ</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/dayz.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">daUgSqpG</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+									        <div id="block4">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Valheim</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/valheim.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">vKgLhEmI</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+									         <div id="block5">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">7 Days to Die</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/7.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">7xfadepS</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+									       <div id="block6">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Euro Truck Simulator 2</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/euro.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">euTRack2</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+                                           
+                                              <div id="block7">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Raft</span></p></th>
+									    <th><p>Цена: 80 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/raft.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">msQPhmgr</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+                                            
+                                            <div id="block8">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>Grand Theft Auto V</p></th>
+									    <th><p>Цена: 200 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/gta.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">gtrSphtM</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+                                               
+
+                                                 <div id="block9">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">DARK SOULS III</span></p></th>
+									    <th><p>Цена: 75 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/dark.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">drkSolc3</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									         <div id="block10">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>Deadside</p></th>
+									    <th><p>Цена: 125 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/deadside.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">dopSide</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+                                               
+
+
+                                                 <div id="block11">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Resident Evil 3</span></p></th>
+									    <th><p>Цена: 200 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/evil3.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">ev3pkS1</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+									         <div id="block12">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>Hunt: Showdown</p></th>
+									    <th><p>Цена: 350 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/hunt.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">hdwson2801s</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									       <div id="block13">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Ghostrunner</span></p></th>
+									    <th><p>Цена: 250 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/ghost.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">ghjosak2181</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+									         <div id="block14">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>Cyberpunk 2077</p></th>
+									    <th><p>Цена: 500 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/cyber.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">cypyrasdASp</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									        <div id="block15">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Outlast</span></p></th>
+									    <th><p>Цена: 50 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/outlast.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">lastout21SA</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									       <div id="block16">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>IN SILENCE</p></th>
+									    <th><p>Цена: 50 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/sl.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">outSlNs32</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+
+
+									        <div id="block17">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Arma 3</span></p></th>
+									    <th><p>Цена: 300 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/arma.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">5gApskp21</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									        <div id="block18">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>theHunter: Call of the Wild™</p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/hunter.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">h2Spn56S</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									        <div id="block19">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3">Hurtworld</span></p></th>
+									    <th><p>Цена: 100 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/hurt.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">mg5pl1gs</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                             </td>
+									  </tr>
+									    </table>
+									       </div>
+
+									         <div id="block20">
+                                           <table border="1">
+									   <tr>
+									    <th><p><span class="colortext3"></span>No Man's Sky</p></th>
+									    <th><p>Цена: 500 рублей</p></th>
+									   </tr>
+									   <tr>
+									    <td><img src="img/sky.jpg" width="400" height="200"></td>
+									    <td><p><span class="colortext2">Код</span> к покупке: <span class="colortext">kJslPmsk</span></p>
+                                            <p>Как купить смотрите <a href="#">тут.</a>.
+                                         </td>
+									  </tr>
+									    </table>
+									       </div>
+                                
+                                
+                                
+
+
+
+
+                          
+
+                          
+               
+
+                             
+
+					</body>
+</html>
